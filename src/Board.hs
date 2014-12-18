f41916996bb6dcee98496e8b6e43a83346b3c8f6
Matt Donnelly <mattdonnelly@me.com>
2014-12-18 02:36:59 +0000

    Remove mine positions vector for now

--- src/Board.hs
+++ src/Board.hs
@@ -1,23 +1,16 @@
-module Board where
+module Board (Board, boardCells, initBoard) where
 
 import           Cell                  (Cell, initCell)
 import           Data.Vector           (Vector)
 import qualified Data.Vector as Vector (empty, replicate, toList)
 
-type Pos = (Int, Int)
-
-data Board = Board
-    { boardMines :: Vector Pos
-    , boardCells :: Vector (Vector Cell)
-    }
+data Board = Board { boardCells :: Vector (Vector Cell) }
 
 instance Show Board where
     show = unlines . map (concatMap show . Vector.toList) . Vector.toList . boardCells
 
 initBoard :: Int -> Int -> Board
-initBoard w h = Board { boardMines = Vector.empty
-                      , boardCells = board
-                      }
+initBoard w h = Board { boardCells = board }
     where
         rows  = Vector.replicate h initCell
         board = Vector.replicate w rows
