d6295d5679b5c12fb1e04c68182cf2fc657030a7
Matt Donnelly <mattdonnelly@me.com>
2014-12-21 17:49:04 +0000

    Reorded isAdjacent function slightly

--- src/Board.hs
+++ src/Board.hs
@@ -37,9 +37,9 @@ countAdjacentMines mines c
         t = Vector.tail mines
 
 isAdjacent :: Cell -> Cell -> Bool
-isAdjacent c1 c2 = dist' == 1
+isAdjacent c1 c2 = floored == 1
     where
         x' = c1 ^. xpos - c2 ^. xpos
         y' = c1 ^. ypos - c2 ^. ypos
-        dist = fromIntegral (x'*x' + y'*y')
-        dist' = (floor :: Double -> Int) $ (sqrt :: Double -> Double) dist
+        dist = (sqrt :: Double -> Double) $ fromIntegral (x'*x' + y'*y')
+        floored = (floor :: Double -> Int) dist
