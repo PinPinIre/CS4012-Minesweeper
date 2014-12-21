2aecd5911e8cb36822464768d1b9ee35fff8b32b
Cathal Geoghegan <pinpinire@gmail.com>
2014-12-21 19:48:27 +0000

    Update GUI code to render mineboard

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,23 @@
 module Main where
 
 import Game
+import Cell
 import Control.Monad.State
+import Control.Monad (liftM2)
 import Graphics.UI.WX
 import Graphics.UI.WXCore
-import System.Random
-import Control.Lens
+import Data.List (groupBy)
+import qualified Data.Function as DF (on)
 
-height   :: Int
-height   = 300
-
-width    :: Int
-width    = 300
+button_width = 30
+button_height = 30
 
 main :: IO ()
 main = do
     let minesweeper = initMinesweeper
     let finished = execState testRun minesweeper
     print finished
+    start gui
 
 testRun :: State Minesweeper Status
 testRun = do
@@ -27,40 +27,59 @@ testRun = do
 
 gui :: IO ()
 gui = do
-    g    <- getStdGen
-    game <- varCreate $ initMinesweeper
-    board<- varGet game
-    let
-        button_width = 50
-        button_height = 50
-        cells = gameState board
+    game    <- varCreate $ initMinesweeper
 
-    f     <- frame    [text          := "Minesweeper!"
-                      , bgcolor      := white
-                      , resizeable   := False]
+    let
+        cells = groupRows $ liftM2 (,) [0..19] [0..19]
+
+    f   <- frame [text          := "Minesweeper!"
+                 , bgcolor      := white ]
+    p   <- panel f []
     status <- statusField [text := "Welcome to Minesweeper"]
-    Graphics.UI.WX.set f [statusBar := [status]]
-    pp <- panel f []
-    bb <- genBoard f cells
+    bb <- genBoard p game cells
     let
         bblayout = layoutBoard bb
-    Graphics.UI.WX.set f [ layout          := bblayout
-                         , bgcolor         := white ]
+    Graphics.UI.WX.set f [ statusBar := [status]
+                         , layout := widget p ]
 
-genBoard :: Frame() -> [[Cell]] -> IO [[(Button ())]]
-genBoard f cells = mapM (genRow f) cells
+genBoard :: Panel() -> Var Minesweeper -> [[(Int, Int)]] -> IO [[(Button ())]]
+genBoard f game cells  = mapM (genRow f game) cells
 
-genRow :: Frame() -> [Cell] -> IO [(Button ())]
-genRow f cells =  mapM (genButtton f) cells
+genRow :: Panel() -> Var Minesweeper -> [(Int, Int)] -> IO [(Button ())]
+genRow f game cells =  mapM (genButtton f game) cells
 
-genButtton :: Frame() -> Cell -> IO (Button ())
-genButtton f c
-                | (c ^. revealed) && (c ^. adjacentMines == 0) = do
-                    button f [ text  := show c]
-                | c ^. flagged = do
-                    button f [ text  := show c]
-                | otherwise = do
-                    button f [ text  := " "]
+genButtton :: Panel() -> Var Minesweeper -> (Int, Int) -> IO (Button ())
+genButtton f game c = do
+    state <- varGet game
+    let
+        (status, newstate) = runState (getCellField mined c) state
+        (x, y) = c
+        xpos = x * button_height
+        ypos = y * button_width
+    varSet game newstate
+    b <- button f [ text  := ""
+                  , position := pt ypos xpos
+                  , size := sz button_width button_height]
+    Graphics.UI.WX.set b [ on click  := reveal f c game b
+                         , on clickRight := flag f c game b]
+    return b
+
+reveal f c game b _ = do
+    state <- varGet game
+    let
+        (status, newstate) = runState (setRevealed c) state
+    Graphics.UI.WX.set b [ text  := "R" ]
+    print newstate
+    varSet game newstate
+
+flag f c game b _ = do
+    state <- varGet game
+    let
+        (status, newstate) = runState (setFlagged c) state
+    Graphics.UI.WX.set b [ text  := "F" ]
+    print newstate
+    varSet game newstate
+
 
 layoutBoard :: [[(Button ())]] -> Layout
 layoutBoard b = grid 0 0 $ map layoutRow b
@@ -68,11 +87,4 @@ layoutBoard b = grid 0 0 $ map layoutRow b
 layoutRow :: [(Button ())] -> [Layout]
 layoutRow r = [row 0 $ map widget r]
 
-addButtton :: Frame() -> [Char] -> Int -> Int -> Int -> Int -> IO (Button ())
-addButtton parent string x y button_width button_height = do
-        let
-            xpos = x * button_height
-            ypos = y * button_width
-        button parent [ text  := string
-                      , position := pt ypos xpos
-                      , size  := sz button_height button_width]
+groupRows = groupBy ((==) `DF.on` fst)
