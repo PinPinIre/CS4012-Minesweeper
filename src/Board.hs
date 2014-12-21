cb9565feb3103f967f9568406bd6e33b6e4695bd
Matt Donnelly <mattdonnelly@me.com>
2014-12-21 18:56:51 +0000

    Cleaned up code for adding mines and added width / height to board record

--- src/Board.hs
+++ src/Board.hs
@@ -8,7 +8,11 @@ import Data.Vector (Vector)
 import qualified Data.Vector as Vector
 import System.Random
 
-data Board = Board { _cells :: Vector (Vector Cell) }
+data Board = Board
+    { _cells :: Vector (Vector Cell)
+    , _width :: Int
+    , _height :: Int
+    }
 
 makeLenses ''Board
 
@@ -16,26 +20,28 @@ instance Show Board where
     show = unlines . map (concatMap show . Vector.toList) . Vector.toList . _cells
 
 initBoard :: Int -> Int -> Int -> StdGen -> Board
-initBoard w h mines rng = Board adjacencyBoard
+initBoard w h mines rng = adjacencyBoard
     where
-        board = Vector.fromList [Vector.fromList [initCell y x | x <- [0..(w - 1)]] | y <- [0..(h - 1)]]
-        minedBoard = addMines 10 w h rng board
+        boardCells = [[initCell y x | x <- [0..(w - 1)]] | y <- [0..(h - 1)]]
+        board = Board (Vector.fromList $ map Vector.fromList boardCells) w h
+        minedBoard = addMines mines rng board
         adjacencyBoard = calculateAdjacency minedBoard
 
-addMines :: Int -> Int -> Int -> StdGen -> Vector (Vector Cell) -> Vector (Vector Cell)
-addMines 0 _ _ _ b = b
-addMines count w h rng b = addMines (count-1) w h newRng' newBoard
+addMines :: Int -> StdGen -> Board -> Board
+addMines 0 _ board = board
+addMines count rng board = addMines (count-1) newRng' newBoard
     where
-        (x, newRng) = randomR (0, w-1) rng
-        (y, newRng') = randomR (0, h-1) newRng
-        newBoard = b & (element y . element x . mined) .~ True
+        (x, newRng) = randomR (0, board ^. width - 1) rng
+        (y, newRng') = randomR (0, board ^. height - 1) newRng
+        newBoard = board & (cells . element y . element x . mined) .~ True
 
-calculateAdjacency :: Vector (Vector Cell) -> Vector (Vector Cell)
-calculateAdjacency board = adjacent
+calculateAdjacency :: Board -> Board
+calculateAdjacency board = board & cells .~ adjacent
     where
+        boardCells = _cells board
         onlyMinesFilter = Vector.filter _mined
-        mines = Vector.concatMap onlyMinesFilter board
-        adjacent = Vector.map (Vector.map (countAdjacentMines mines)) board
+        mines = Vector.concatMap onlyMinesFilter boardCells
+        adjacent = Vector.map (Vector.map (countAdjacentMines mines)) boardCells
 
 countAdjacentMines :: Vector Cell -> Cell -> Cell
 countAdjacentMines mines c
