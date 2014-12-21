ac6616c245748acb4203a1219046d52578042125
Cathal Geoghegan <pinpinire@gmail.com>
2014-12-21 13:22:20 +0000

    Add functions to generate UI from a game

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,69 @@
 module Main where
 
 import Minesweeper
+import Cell
+import Graphics.UI.WX
+import Graphics.UI.WXCore
+import System.Random
+import Control.Lens
+
+height   :: Int
+height   = 300
+
+width    :: Int
+width    = 300
 
 main :: IO ()
-main = do
-    let minesweeper = initMinesweeper
-    let won = gameStatus minesweeper
-    print minesweeper
-    print won
+main = start gui
+
+gui :: IO ()
+gui = do
+    g    <- getStdGen
+    game <- varCreate $ initMinesweeper
+    board<- varGet game
+    let
+        button_width = 50
+        button_height = 50
+        cells = gameState board
+
+    f     <- frame    [text          := "Minesweeper!"
+                      , bgcolor      := white
+                      , resizeable   := False]
+    status <- statusField [text := "Welcome to Minesweeper"]
+    Graphics.UI.WX.set f [statusBar := [status]]
+    pp <- panel f []
+    bb <- genBoard f cells
+    let
+        bblayout = layoutBoard bb
+    Graphics.UI.WX.set f [ layout          := bblayout
+                         , bgcolor         := white ]
+
+genBoard :: Frame() -> [[Cell]] -> IO [[(Button ())]]
+genBoard f cells = mapM (genRow f) cells
+
+genRow :: Frame() -> [Cell] -> IO [(Button ())]
+genRow f cells =  mapM (genButtton f) cells
+
+genButtton :: Frame() -> Cell -> IO (Button ())
+genButtton f c
+                | (c ^. revealed) && (c ^. adjacentMines == 0) = do
+                    button f [ text  := show c]
+                | c ^. flagged = do
+                    button f [ text  := show c]
+                | otherwise = do
+                    button f [ text  := " "]
+
+layoutBoard :: [[(Button ())]] -> Layout
+layoutBoard b = grid 0 0 $ map layoutRow b
+
+layoutRow :: [(Button ())] -> [Layout]
+layoutRow r = [row 0 $ map widget r]
+
+addButtton :: Frame() -> [Char] -> Int -> Int -> Int -> Int -> IO (Button ())
+addButtton parent string x y button_width button_height = do
+        let
+            xpos = x * button_height
+            ypos = y * button_width
+        button parent [ text  := string
+                      , position := pt ypos xpos
+                      , size  := sz button_height button_width]
