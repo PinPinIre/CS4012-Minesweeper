4100401bbea30f8c6b6ace2d196c491c47e7b7a6
Matt Donnelly <mattdonnelly@me.com>
2014-12-21 01:11:05 +0000

    Finished revealCell function and added flagCell function

--- src/Game.hs
+++ src/Game.hs
@@ -9,10 +9,6 @@ import Control.Monad.State
 import Data.Maybe
 import qualified Data.Vector as Vector (concat, toList)
 
-type Pos = (Int, Int)
-
-data Status = Won | Lose | Move deriving (Show, Eq)
-
 data Minesweeper = Minesweeper
     { _board :: Board
     , _remainingFlags :: Int
@@ -23,33 +19,34 @@ makeLenses ''Minesweeper
 instance Show Minesweeper where
     show = show . view board
 
+type Pos = (Int, Int)
+
 type Game = State Minesweeper
+
+data Status = Won | Lose | Move deriving (Show, Eq)
 
 initMinesweeper :: Minesweeper
 initMinesweeper = Minesweeper { _board = initBoard 20 20
                               , _remainingFlags = 10
                               }
 
-checkStatus :: Game Status
-checkStatus = do
+isWon :: Game Bool
+isWon = do
     m <- get
+
     let boardCells = m ^. board . cells
     let concatCells = Vector.toList $ Vector.concat $ Vector.toList boardCells
     let cellStates = map checkCellStatus concatCells
 
-    if all (==Won) cellStates then
-        return Won
-    else if Lose `elem` cellStates then
-        return Lose
+    if all (==True) cellStates then
+        return True
     else
-        return Move
+        return False
 
-checkCellStatus :: Cell -> Status
+checkCellStatus :: Cell -> Bool
 checkCellStatus c
-    | c ^. flagged && c ^. mined = Won
-    | c ^. revealed && c ^. mined = Lose
-    | otherwise = Move
-
+    | c ^. flagged && c ^. mined = True
+    | otherwise                  = False
 
 revealCell :: Pos -> Game Status
 revealCell p = do
@@ -58,6 +55,30 @@ revealCell p = do
         m <- isMined p
         if m then
             return Lose
+        else do
+            setRevealed p
+            won <- isWon
+
+            if won then
+                return Won
+            else
+                return Move
+    else
+        return Move
+
+flagCell :: Pos -> Game Status
+flagCell p = do
+    m <- get
+    if m ^. remainingFlags > 0 then do
+        f <- isFlagged p
+        if not f then do
+            setFlagged p
+            won <- isWon
+
+            if won then
+                return Won
+            else
+                return Move
         else
             return Move
     else
@@ -69,8 +90,14 @@ isMined = getCellField mined
 isFlagged :: Pos -> Game Bool
 isFlagged = getCellField flagged
 
+setFlagged :: Pos -> Game ()
+setFlagged = setCellField flagged True
+
 isRevealed :: Pos -> Game Bool
 isRevealed = getCellField revealed
+
+setRevealed :: Pos -> Game ()
+setRevealed = setCellField revealed True
 
 getAdjacentMines :: Pos -> Game Int
 getAdjacentMines = getCellField adjacentMines
@@ -79,3 +106,8 @@ getCellField :: Getter Cell a -> Pos -> Game a
 getCellField getter (x, y) = do
     m <- get
     return $ fromJust $ m ^? (board . cells . element y . element x . getter)
+
+setCellField :: Setter Cell Cell a b -> b -> Pos -> Game ()
+setCellField setter val (x, y)  = do
+    let new = board . cells . element x . element y . setter %~ const val
+    modify new
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
 import Game
+import Control.Monad.State
 
 main :: IO ()
 main = do
     let minesweeper = initMinesweeper
-    print minesweeper
+    let (_, finished) = runState testRun minesweeper
+    print finished
+
+testRun :: State Minesweeper Status
+testRun = do
+    revealCell (1, 2)
+    revealCell (2, 7)
+    flagCell (5, 8)
