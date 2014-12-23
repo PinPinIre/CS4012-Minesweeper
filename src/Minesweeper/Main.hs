f20fd63a8dfb75252d708ac2cb8395b0a6351574
Matt Donnelly <mattdonnelly@me.com>
2014-12-23 18:36:21 +0000

    Rename Minesweeper type to Game and improved logic for flagging and revealing cells

--- src/Minesweeper/Main.hs
+++ src/Minesweeper/Main.hs
@@ -25,13 +25,13 @@ gui :: IO ()
 gui = do
     rng <- newStdGen
 
-    let minesweeper = initMinesweeper rng
-    g <- varCreate minesweeper
+    let game = initGame rng
+    g <- varCreate game
 
     f <- frame [ text := "Minesweeper!"
                , bgcolor := white]
 
-    let numFlags = minesweeper ^. remainingFlags
+    let numFlags = game ^. remainingFlags
     l <- staticText f [ text := ("Remaining Flags: " ++ show numFlags) ]
 
     p <- panel f []
@@ -40,7 +40,7 @@ gui = do
 
     set f [ layout := margin 5 $ column 5 [floatTop $ widget p, floatLeft $ widget l] ]
 
-genBoard :: Panel () -> StaticText () -> Var Minesweeper -> IO ()
+genBoard :: Panel () -> StaticText () -> Var Game -> IO ()
 genBoard p st g = do
     b <- varGet g
 
@@ -49,7 +49,7 @@ genBoard p st g = do
 
     mapM_ (mapM (genButtton p st g)) cellsList
 
-genButtton :: Panel () -> StaticText () -> Var Minesweeper -> Cell -> IO (Button ())
+genButtton :: Panel () -> StaticText () -> Var Game -> Cell -> IO (Button ())
 genButtton p st g c = do
     let x = _xpos c
         y = _ypos c
@@ -63,70 +63,64 @@ genButtton p st g c = do
 
     return b
 
-reveal :: Int -> Int -> Var Minesweeper -> Button () -> Point -> IO ()
+reveal :: Int -> Int -> Var Game -> Button () -> Point -> IO ()
 reveal x y game b _ = do
     g <- varGet game
     newState <- execStateT (revealGame x y b) g
     varSet game newState
 
-revealGame :: Int -> Int -> Button () -> Game ()
+revealGame :: Int -> Int -> Button () -> GameState ()
 revealGame x y b = do
-    status <- revealCell x y
+    status <- toggleRevealCell x y
 
     case status of
+        Move -> do
+            adj <- getAdjacentMines x y
+            liftIO $ set b [ text := show adj ]
+
         Lose -> do
             liftIO $ set b [ text  := "💣" ]
             fr <- liftIO $ frame [ text := "Minesweeper!" ]
             l <- liftIO $ staticText fr [ text := "You Lose!"]
             liftIO $ set fr [ layout := margin 100 $ floatCenter $ widget l ]
+
+        _ -> return ()
+
+    newState <- State.get
+    liftIO $ print newState
+
+flag :: Int -> Int -> StaticText () -> Var Game -> Button () -> Point -> IO ()
+flag x y st game b _ = do
+    g <- varGet game
+    newState <- execStateT (flagGame x y st b) g
+    varSet game newState
+
+flagGame :: Int -> Int -> StaticText () -> Button () -> GameState ()
+flagGame x y st b = do
+    status <- toggleFlagCell x y
+
+    case status of
+        Move -> do
+            numFlags <- use remainingFlags
+            liftIO $ set st [ text := ("Remaining Flags: " ++ show numFlags) ]
+
+            f <- isFlagged x y
+            if f then
+                liftIO $ set b [ text := "🚩" ]
+            else
+                liftIO $ set b [ text := "" ]
+
+        Won -> liftIO $ do
+            fr <- liftIO $ frame [ text := "Minesweeper!"
+                                 , bgcolor := white]
+            l <- liftIO $ staticText fr [ text := "You won!"]
+            liftIO $ set fr [ layout := widget l ]
+
+        Error -> return ()
+
         _ -> do
             adj <- getAdjacentMines x y
             liftIO $ set b [ text := show adj ]
 
     newState <- State.get
     liftIO $ print newState
-
-flag :: Int -> Int -> StaticText () -> Var Minesweeper -> Button () -> Point -> IO ()
-flag x y st game b _ = do
-    g <- varGet game
-    newState <- execStateT (flagGame x y st b) g
-    varSet game newState
-
-flagGame :: Int -> Int -> StaticText () -> Button () -> Game ()
-flagGame x y st b = do
-    r <- isRevealed x y
-    f <- isFlagged x y
-
-    case (r, f) of
-        (False, False) -> do
-            status <- flagCell x y
-            case status of
-                Move -> do
-                    numFlags <- use remainingFlags
-                    liftIO $ set st [ text := ("Remaining Flags: " ++ show numFlags) ]
-                    liftIO $ set b [ text := "🚩" ]
-
-                    newState <- State.get
-                    liftIO $ print newState
-
-                OutOfFlags -> return ()
-
-                Won -> liftIO $ do
-                    fr <- liftIO $ frame [ text := "Minesweeper!"
-                                         , bgcolor := white]
-                    l <- liftIO $ staticText fr [ text := "You won!"]
-                    liftIO $ set fr [ layout := widget l ]
-
-                _ -> do
-                    adj <- getAdjacentMines x y
-                    liftIO $ set b [ text := show adj ]
-
-        (False, True) -> do
-            unflagCell x y
-            numFlags <- use remainingFlags
-            liftIO $ set st [ text := ("Remaining Flags: " ++ show numFlags) ]
-            liftIO $ set b [ text  := " " ]
-
-            newState <- State.get
-            liftIO $ print newState
-        _ -> return ()
